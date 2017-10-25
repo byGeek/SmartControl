dd54aff69ecefd11a9390962cb394866156bfb6f
byGeek <byyang@mail.dlut.edu.cn>
2017-10-25 14:37:07 +0800

    change server side to a system tray app

--- Server/SmartControlServer/App.xaml.cs
+++ Server/SmartControlServer/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Forms = System.Windows.Forms;
 
 namespace SmartControlServer
 {
@@ -13,5 +14,110 @@ namespace SmartControlServer
     /// </summary>
     public partial class App : Application
     {
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private Controller _serverController;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            _serverController = new Controller();
+
+            CreateContextMenu();
+
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = (Exception)e.ExceptionObject;
+            System.Diagnostics.Debug.WriteLine(exception.Message);
+            System.Diagnostics.Debug.WriteLine(exception.StackTrace);
+
+            MessageBox.Show("Exception occurred! Application will be closed");
+        }
+
+        private Forms.ToolStripMenuItem _startMenuItem;
+        private Forms.ToolStripMenuItem _stopMenuItem;
+        private Forms.ToolStripMenuItem _exitMenuItem;
+
+        private void CreateContextMenu()
+        {
+            if(_notifyIcon == null)
+            {
+                _notifyIcon = new System.Windows.Forms.NotifyIcon();
+                _notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+
+                _startMenuItem = new Forms.ToolStripMenuItem("Start Server");
+                _startMenuItem.Click += _startMenuItem_Click;
+                _notifyIcon.ContextMenuStrip.Items.Add(_startMenuItem);
+
+                _stopMenuItem = new Forms.ToolStripMenuItem("Stop Server");
+                _stopMenuItem.Click += _stopMenuItem_Click;
+                _notifyIcon.ContextMenuStrip.Items.Add(_stopMenuItem);
+
+                var seperator = new Forms.ToolStripSeparator();
+                _notifyIcon.ContextMenuStrip.Items.Add(seperator);
+
+                _exitMenuItem = new Forms.ToolStripMenuItem("Exit");
+                _exitMenuItem.Click += _exitMenuItem_Click;
+                _notifyIcon.ContextMenuStrip.Items.Add(_exitMenuItem);
+
+                _notifyIcon.ContextMenuStrip.Opening += ContextMenuStrip_Opening;
+                _notifyIcon.Icon = SmartControlServer.Properties.Resources.trayIcon;
+                _notifyIcon.Visible = true;
+
+                var text = "Server IP: " + _serverController.IP;
+                _notifyIcon.Text = text;
+                _notifyIcon.ShowBalloonTip(3 * 1000, "Info", text, Forms.ToolTipIcon.Info);
+            }
+        }
+
+        void ContextMenuStrip_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SetMenuItem();
+        }
+
+        void _exitMenuItem_Click(object sender, EventArgs e)
+        {
+            _serverController.Dispose();
+            Application.Current.Shutdown();
+        }
+
+        void _stopMenuItem_Click(object sender, EventArgs e)
+        {
+            _serverController.StopServer();
+            _notifyIcon.ShowBalloonTip(2 * 1000, "Info", "Server stopped", Forms.ToolTipIcon.Info);
+        }
+
+        void _startMenuItem_Click(object sender, EventArgs e)
+        {
+            _serverController.StartServer();
+            _notifyIcon.ShowBalloonTip(2 * 1000, "Info", "Server started", Forms.ToolTipIcon.Info);
+            
+        }
+
+        void SetMenuItem()
+        {
+            var status = _serverController.Status;
+            switch (status)
+            {
+                case ServerStatus.Running:
+                    _startMenuItem.Enabled = false;
+                    _stopMenuItem.Enabled = true;
+                    _exitMenuItem.Enabled = true;
+                    break;
+                case ServerStatus.Idle:
+                    _startMenuItem.Enabled = true;
+                    _stopMenuItem.Enabled = false;
+                    _exitMenuItem.Enabled = true;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+
+    
 }
---
+++ Server/SmartControlServer/Controller.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+
+namespace SmartControlServer
+{
+    internal class Controller
+    {
+        private SocketServerHost serverHost;
+        private Thread serverThread;
+        private ServerStatus _status;
+        public ServerStatus Status
+        {
+            get { return _status; }
+        }
+
+        private string _ip;
+        public string IP
+        {
+            get { return _ip; }
+        }
+
+        public Controller()
+        {
+            serverHost = new SocketServerHost();
+            _ip = GetLocalIpAddress();
+        }
+        public void StartServer()
+        {
+            serverThread = new Thread(serverHost.StartListening);
+            serverThread.IsBackground = true;
+            serverThread.Start();
+            _status = ServerStatus.Running;
+        }
+
+        public void StopServer()
+        {
+            if (serverHost != null)
+            {
+                serverHost.StopListening();
+            }
+
+            if (serverThread != null && serverThread.IsAlive)
+            {
+                //TODO user flag
+                serverThread.Abort();
+            }
+            _status = ServerStatus.Idle;
+        }
+
+        public void Dispose()
+        {
+            if (serverHost != null)
+            {
+                serverHost.Dispose();
+            }
+            if (serverThread != null)
+            {
+                serverThread.Abort();
+            }
+        }
+
+        private string GetLocalIpAddress()
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                return "Network unavailabel";
+            }
+            IPHostEntry entry = Dns.GetHostEntry(Dns.GetHostName());
+
+            var ip = entry.AddressList.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            if (ip == null)
+            {
+                return "Can not get ip address";
+            }
+            else
+            {
+                return ip.ToString();
+            }
+        }
+    }
+
+    internal enum ServerStatus
+    {
+        Idle,
+        Running
+    }
+}
