03ad2876181de86af65a00e6cc04aab2b9f56075
byGeek <byyang@mail.dlut.edu.cn>
2017-10-25 16:25:56 +0800

    use nuget pakage InputSimulator to simulater the mouse scroll

--- Server/SmartControlServer/MessageSender.cs
+++ Server/SmartControlServer/MessageSender.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Diagnostics;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace SmartControlServer
 {
@@ -33,29 +35,8 @@ namespace SmartControlServer
 
         private void ScrollView(int pos)
         {
-            //chrome receive the message but will not scroll
-            //IntPtr hwnd = _msgHelper.getWindowPtr("Chrome_WidgetWin_1", "TestStack.White - Google Chrome");
-            //IntPtr objWnd = _msgHelper.getChildObjPtr(IntPtr.Zero, IntPtr.Zero, "Chrome_WidgetWin_1", null);
-
-            //Point p = new Point();
-            //_msgHelper.sendWindowsMessage(objWnd, MessageHelper.WM_MOUSEWHEEL,
-            //    (MessageHelper.WHEEL_DELTA * pos) << 16, ref p);
-
-            //can not debug, because when you debug your app, the foregroundwindow will
-            //always be visual studio
-            //var hwnd = MessageHelper.GetForegroundWindow();
-            //Point p = new Point(0, 0);
-            //_msgHelper.sendWindowsMessage(hwnd, MessageHelper.WM_MOUSEWHEEL,
-                //(MessageHelper.WHEEL_DELTA * pos) << 16, ref p);
-
-            //send to notepad
-            //TODO SET a ui parameter
-            IntPtr hwnd = _msgHelper.getWindowPtr(null, "smart_control_demo.txt - Notepad");
-            IntPtr objWnd = _msgHelper.getChildObjPtr(hwnd, IntPtr.Zero, "Edit", null);
-            Point p = new Point(0, 0);
-
-            _msgHelper.sendWindowsMessage(objWnd, MessageHelper.WM_MOUSEWHEEL,
-                (MessageHelper.WHEEL_DELTA * pos) << 16, ref p);
+            var sim = new InputSimulator();
+            sim.Mouse.VerticalScroll(pos);
         }
     }
 }
